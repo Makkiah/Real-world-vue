36af948134d00e09142a4622843548a995e92e01
Makkiah Muhammad <makkiahf@gmail.com>
2022-06-23 13:11:08 -0400

    Vuex Fin

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -4,11 +4,9 @@
     :to="{ name: 'event-show', params: { id: event.id } }"
   >
     <div class="event-card -shadow">
-      <span class="eyebrow">@{{ event.time }} on {{ event.date }}.</span>
-      <h4>{{ event.title }}</h4>
-      <BaseIcon name="users"
-        >{{ event.attendees ? event.attendees.length : 0 }} attending</BaseIcon
-      >
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <BaseIcon name="users">{{ event.attendees.length }} attending</BaseIcon>
     </div>
   </router-link>
 </template>
@@ -35,7 +33,6 @@ export default {
 .event-card > .title {
   margin: 0;
 }
-
 .event-link {
   color: black;
   text-decoration: none;
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -3,16 +3,22 @@
     <div class="event-header">
       <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
       <h1 class="title">{{ event.title }}</h1>
-      <h5>Organized by {{ event.organizer }}</h5>
+      <h5>Organized by {{ event.organizer ? event.organizer.name : '' }}</h5>
       <h5>Category: {{ event.category }}</h5>
     </div>
+
     <BaseIcon name="map"><h2>Location</h2></BaseIcon>
+
     <address>{{ event.location }}</address>
+
     <h2>Event details</h2>
     <p>{{ event.description }}</p>
+
     <h2>
       Attendees
-      <span class="badge -fill-gradient">{{ event.attendees.length }}</span>
+      <span class="badge -fill-gradient">{{
+        event.attendees ? event.attendees.length : 0
+      }}</span>
     </h2>
     <ul class="list-group">
       <li
@@ -25,29 +31,20 @@
     </ul>
   </div>
 </template>
-
 <script>
-import EventService from '@/services/EventService'
+import { mapState, mapActions } from 'vuex'
 
 export default {
   props: ['id'],
-  data() {
-    return {
-      event: {},
-    }
-  },
   created() {
-    EventService.getEvent(this.id)
-      .then((response) => {
-        this.event = response.data
-      })
-      .catch((error) => {
-        console.log('There was an error', error.response)
-      })
+    this.fetchEvent(this.id)
   },
+  computed: mapState({
+    event: (state) => state.event.event,
+  }),
+  methods: mapActions('event', ['fetchEvent']),
 }
 </script>
-
 <style scoped>
 .location {
   margin-bottom: 0;
